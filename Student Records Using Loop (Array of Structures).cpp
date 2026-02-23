99730eebe51627a9fc44852bf5ba5e52311573fb
MMoorer7 <mmoorer7@aum.edu>
2026-02-23 15:39:18 -0600

    Add files via upload

---
+++ Student Records Using Loop (Array of Structures).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+struct Student
+{
+    int roll_number;
+    char name[50];
+    float marks;
+};
+
+int main()
+{
+    Student students[3];
+
+    for (int index = 0; index < 3; index++)
+    {
+        cout << "\n--- Enter details for Student " << (index + 1) << " ---\n";
+
+        cout << "Enter roll number: ";
+        cin >> students[index].roll_number;
+
+        cin.ignore(); 
+
+        cout << "Enter name: ";
+        cin.getline(students[index].name, 50);
+
+        cout << "Enter marks: ";
+        cin >> students[index].marks;
+    }
+
+    cout << "\n===== Student Details =====\n";
+
+    for (int index = 0; index < 3; index++)
+    {
+        cout << "\nStudent " << (index + 1) << endl;
+        cout << "Roll No: " << students[index].roll_number << endl;
+        cout << "Name: " << students[index].name << endl;
+        cout << "Marks: " << students[index].marks << endl;
+    }
+
+    return 0;
+}
