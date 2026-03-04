b30118853200d0a0f3b2bbba930428a6eb12e4dc
MMoorer7 <mmoorer7@aum.edu>
2026-03-04 16:29:07 -0600

    Add files via upload

---
+++ chp 5 pract 45 Hospital Patient Records.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+struct Patient
+{
+    int patient_id;
+    char name[50];
+    float temperature;
+};
+
+int main()
+{
+    Patient patients[5];
+
+    for (int index = 0; index < 5; index++)
+    {
+        cout << "\nEnter details for Patient " << index + 1 << endl;
+
+        cout << "Patient ID: ";
+        cin >> patients[index].patient_id;
+
+        cin.ignore();   
+
+        cout << "Patient Name: ";
+        cin.getline(patients[index].name, 50);
+
+        cout << "Body Temperature: ";
+        cin >> patients[index].temperature;
+    }
+
+    cout << "\n===== Patient Records =====\n";
+
+    for (int index = 0; index < 5; index++)
+    {
+        cout << "\nPatient " << index + 1 << endl;
+        cout << "ID: " << patients[index].patient_id << endl;
+        cout << "Name: " << patients[index].name << endl;
+        cout << "Temperature: " << patients[index].temperature << endl;
+    }
+
+    return 0;
+}
